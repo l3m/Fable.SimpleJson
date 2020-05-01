41f8c852c5d77097a2a0338e871b6cc145467c7c
Jonas Bösch <jb@vlot.ch>
2020-05-01 14:42:08 +0200

    Migrate to Fake 5
    + add sln
    + local dotnet tools
    + add package.json task for fake build
    + updated headless to netcoreapp3.1

--- build.fsx
+++ build.fsx
@@ -1,31 +1,50 @@
-#r @"packages/build/FAKE/tools/FakeLib.dll"
+#r "paket: groupref build //"
+#load "./.fake/build.fsx/intellisense.fsx"
+
+#if !FAKE
+#r "netstandard"
+#endif
 
 open System
 open System.IO
-open Fake
+
+open Fake.Core
+open Fake.DotNet
+open Fake.IO
+open Fake.IO.FileSystemOperators
+ 
+Target.initEnvironment ()
+
 let libPath = "./src"
 let testsPath = "./test"
 
 let platformTool tool winTool =
-  let tool = if isUnix then tool else winTool
-  tool
-  |> ProcessHelper.tryFindFileOnPath
-  |> function Some t -> t | _ -> failwithf "%s not found" tool
-
+    let tool = if Environment.isUnix then tool else winTool
+    match ProcessUtils.tryFindFileOnPath tool with
+    | Some t -> t
+    | _ ->
+        let errorMsg =
+            tool + " was not found in path. " +
+            "Please install it and make sure it's available from your path. " +
+            "See https://safe-stack.github.io/docs/quickstart/#install-pre-requisites for more info"
+        failwith errorMsg
+        
 let nodeTool = platformTool "node" "node.exe"
 let mutable dotnetCli = "dotnet"
 
-let run fileName args workingDir =
-    printfn "CWD: %s" workingDir
-    let fileName, args =
-        if isUnix
-        then fileName, args else "cmd", ("/C " + fileName + " " + args)
-    let ok =
-        execProcess (fun info ->
-             info.FileName <- fileName
-             info.WorkingDirectory <- workingDir
-             info.Arguments <- args) TimeSpan.MaxValue
-    if not ok then failwith (sprintf "'%s> %s %s' task failed" workingDir fileName args)
+let run cmd args workingDir =
+    let arguments = args |> String.split ' ' |> Arguments.OfArgs
+    Command.RawCommand (cmd, arguments)
+    |> CreateProcess.fromCommand
+    |> CreateProcess.withWorkingDirectory workingDir
+    |> CreateProcess.ensureExitCode
+    |> Proc.run
+    |> ignore
+
+let runDotNet cmd workingDir =
+    let result =
+        DotNet.exec (DotNet.Options.withWorkingDirectory workingDir) cmd ""
+    if result.ExitCode <> 0 then failwithf "'dotnet %s' failed in %s" cmd workingDir
 
 let delete file =
     if File.Exists(file)
@@ -40,33 +59,32 @@ let cleanBundles() =
         |> Path.GetFullPath
         |> delete
 
-Target "Clean" <| fun _ ->
-    [ testsPath </> "bin"
-      testsPath </> "obj"
-      libPath </> "bin"
-      libPath </> "obj" ]
-    |> CleanDirs
+let cleanBinObjInPaths paths =  
+    paths
+    |> List.collect (fun path ->
+         [ path </> "bin"
+           path </> "obj" ])
+    |> Shell.cleanDirs
 
+Target.create "Clean" <| fun _ ->
+    cleanBinObjInPaths [testsPath; libPath]
     cleanBundles()
 
-
-
-Target "InstallNpmPackages" (fun _ ->
+Target.create "InstallNpmPackages" (fun _ ->
   printfn "Node version:"
   run "node" "--version" __SOURCE_DIRECTORY__
   run "npm" "install" __SOURCE_DIRECTORY__
 )
 
-Target "RunLiveTests" <| fun _ ->
+Target.create "RunLiveTests" <| fun _ ->
   run "npm" "run start" testsPath
 
 let publish projectPath = fun () ->
-    [ projectPath </> "bin"
-      projectPath </> "obj" ] |> CleanDirs
+    cleanBinObjInPaths [projectPath]
     run dotnetCli "restore --no-cache" projectPath
     run dotnetCli "pack -c Release" projectPath
     let nugetKey =
-        match environVarOrNone "NUGET_KEY" with
+        match Environment.environVarOrNone "NUGET_KEY" with
         | Some nugetKey -> nugetKey
         | None -> failwith "The Nuget API key must be set in a NUGET_KEY environmental variable"
     let nupkg =
@@ -75,18 +93,21 @@ let publish projectPath = fun () ->
         |> Path.GetFullPath
 
     let pushCmd = sprintf "nuget push %s -s nuget.org -k %s" nupkg nugetKey
-    run dotnetCli pushCmd projectPath
+    runDotNet pushCmd projectPath
 
-Target "PublishNuget" (publish libPath)
+Target.create "PublishNuget" <| fun _ ->
+    (publish libPath)()
 
-Target "CompileFableTestProject" <| fun _ ->
+Target.create "CompileFableTestProject" <| fun _ ->
     run "npm" "run build" testsPath
 
-Target "RunTests" <| fun _ ->
+Target.create "RunTests" <| fun _ ->
     printfn "Building %s with Fable" testsPath
     run "npm" "test" "."
     run "npm" "run headless-tests" "."
     cleanBundles()
+
+open Fake.Core.TargetOperators
 
 "Clean"
   ==> "InstallNpmPackages"
@@ -96,4 +117,4 @@ Target "RunTests" <| fun _ ->
  ==> "InstallNpmPackages"
  ==> "RunTests"
 
-RunTargetOrDefault "RunTests"
+Target.runOrDefaultWithArguments "RunTests"
